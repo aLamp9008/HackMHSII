9c81f74c4dc31512a5434825a7cb9ebeabf40f11
Alden Lamp <alamp9008@gmail.com>
2016-05-21 15:27:22 -0400

    Trying to work on the main.storyboard

--- Hack MHS II/ViewController.swift
+++ Hack MHS II/ViewController.swift
@@ -12,20 +12,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         
-        print("works")
         
         
-        print("hello")
+        
         
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
+        
+        
+        
+        
+        
     }
 
 
